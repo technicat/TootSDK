24518a835c84135b16b89ba8db62f602a81f386c
Philip Chu <technicat@users.noreply.github.com>
2024-04-24 09:34:37 -0600

    todo - enforce minimum scheduled post delay

--- Sources/TootSDK/Extensions/TootClient/TootClientPostSchedule.swift
+++ Sources/TootSDK/Extensions/TootClient/TootClientPostSchedule.swift
@@ -5,6 +5,7 @@ extension TootClient {
     /// todo - handle poll
     /// todo - handle pleroma params (contentType and replyToConversationId)
     /// todo - handle idempotency
+    /// todo - option to enforce minimum delay (mastodon is five minutes)
     /// https://docs.joinmastodon.org/methods/statuses/#create
     public func schedule(
         text: String,
