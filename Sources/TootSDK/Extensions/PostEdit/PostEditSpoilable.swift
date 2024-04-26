bfd8b4548a1bf0679a731723d43e445c8035ec49
Philip Chu <technicat@users.noreply.github.com>
2024-04-25 19:16:24 -0600

    Create PostEditSpoilable.swift

---
+++ Sources/TootSDK/Extensions/PostEdit/PostEditSpoilable.swift
@@ -0,0 +1,10 @@
+extension PostEdit: Spoilable {
+
+    public var spoiler: String? {
+        // maybe should check for whitespace
+        guard !spoilerText.isEmpty else {
+            return nil
+        }
+        return spoilerText
+    }
+}
