4a587da466f549924c1ba6d26ce8869445ff5bc8
Philip Chu <technicat@users.noreply.github.com>
2024-05-01 20:58:30 -0600

    post.firstFilter

--- Sources/TootSDK/Extensions/Post/PostFilter.swift
+++ Sources/TootSDK/Extensions/Post/PostFilter.swift
@@ -6,4 +6,18 @@ extension Post {
         }
         return !filtered.isEmpty
     }
+    
+    public var firstFilterResult: FilterResult? {
+        guard let filtered else {
+            return nil
+        }
+        guard !filtered.isEmpty else {
+            return nil
+        }
+        return filtered[0]
+    }
+    
+    public var firstFilter: Filter? {
+        firstFilterResult?.filter
+    }
 }
