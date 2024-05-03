8a27d093a96704ae7934eaf46991515c73e4aee2
Philip Chu <technicat@users.noreply.github.com>
2024-05-03 09:38:40 -0600

    Create TootClientMarker.swift

---
+++ Sources/TootSDK/Extensions/TootClient/TootClientMarker.swift
@@ -0,0 +1,7 @@
+extension TootClient {
+    
+    public func getMarker(for timeline: Marker.Timeline) async throws -> Marker? {
+      let markers = try await getMarkers(for: Set([timeline]))
+      return markers[timeline]
+    }
+}
