db3f9bb3cc715a49e69ff637d7e141c9fc28e71f
Philip Chu <technicat@users.noreply.github.com>
2024-05-05 09:48:43 -0600

    add singular add to list

--- Sources/TootSDK/Extensions/TootClient/TootClientListAdd.swift
+++ Sources/TootSDK/Extensions/TootClient/TootClientListAdd.swift
@@ -7,4 +7,8 @@ extension TootClient {
             id: list.id,
             params: params)
     }
+    
+    public func add(to list: List, account: Account) async throws {
+        try await add(to: list, accounts: [account])
+    }
 }
