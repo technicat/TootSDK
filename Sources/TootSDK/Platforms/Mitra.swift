a0ba994d0c054393af4d657e41ae5782d5961e16
Philip Chu <technicat@users.noreply.github.com>
2024-05-05 19:48:03 -0600

    mitra - lists not supported

--- Sources/TootSDK/Platforms/Mitra.swift
+++ Sources/TootSDK/Platforms/Mitra.swift
@@ -11,6 +11,9 @@ open class Mitra: MastoAPI {
     open override var trendingLinksLimit: Int { 0 }
     open override var trendingPostsLimit: Int { 0 }
     open override var trendingTagsLimit: Int { 0 }
+    
+    /// lists not supported
+    open override var listAccountsPageLimit: Int { 0 }
 
     open override func getLimit(for timeline: Timeline) -> Int {
         switch timeline {
