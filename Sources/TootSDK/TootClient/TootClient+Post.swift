4544bcaebc20a1892a1611a728b1efcfa2a23b61
Philip Chu <technicat@users.noreply.github.com>
2024-05-06 16:47:02 -0600

    comment

--- Sources/TootSDK/TootClient/TootClient+Post.swift
+++ Sources/TootSDK/TootClient/TootClient+Post.swift
@@ -16,6 +16,7 @@ extension TootClient {
         let req = try HTTPRequestBuilder {
             $0.url = getURL(["api", "v1", "statuses"])
             $0.method = .post
+            // Mitra and Sharkey expect json
             $0.body = try .json(params, encoder: self.encoder)
           //  $0.body = try .multipart(params, boundary: UUID().uuidString)
         }
