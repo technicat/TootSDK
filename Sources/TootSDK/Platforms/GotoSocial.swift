e1ec2ee0382a271ee263b9748e69de8754f6e7bb
Philip Chu <technicat@users.noreply.github.com>
2024-05-09 14:18:02 -0600

    make lint

--- Sources/TootSDK/Platforms/GotoSocial.swift
+++ Sources/TootSDK/Platforms/GotoSocial.swift
@@ -3,7 +3,7 @@ open class GotoSocial: MastoAPI {
     open override var name: String { "GotoSocial" }
 
     open override var supportsPublicTimeline: Bool { false }
-    
+
     /// trends not supported
     open override var trendingLinksLimit: Int { 0 }
     open override var trendingPostsLimit: Int { 0 }
