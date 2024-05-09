2b42c26f43359d720cfbb3572fb51826178f7591
Philip Chu <technicat@users.noreply.github.com>
2024-05-09 00:03:28 -0600

    gotosocial doesn't support trends

--- Sources/TootSDK/Platforms/GotoSocial.swift
+++ Sources/TootSDK/Platforms/GotoSocial.swift
@@ -2,4 +2,9 @@ open class GotoSocial: MastoAPI {
     open override var name: String { "GotoSocial" }
 
     open override var supportsPublicTimeline: Bool { false }
+    
+    /// trends not supported
+    open override var trendingLinksLimit: Int { 0 }
+    open override var trendingPostsLimit: Int { 0 }
+    open override var trendingTagsLimit: Int { 0 }
 }
