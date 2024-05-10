be09f006ca9d9cef6a6cf98c0b1402c40036b6ab
Philip Chu <technicat@users.noreply.github.com>
2024-05-09 23:01:14 -0600

    gotosocial doesn't implement  conversations

--- Sources/TootSDK/Platforms/GotoSocial.swift
+++ Sources/TootSDK/Platforms/GotoSocial.swift
@@ -4,6 +4,9 @@ open class GotoSocial: MastoAPI {
 
     open override var supportsPublicTimeline: Bool { false }
 
+    /// conversations not supported
+    open override var conversationsPageLimit: Int { 0 }
+
     /// trends not supported
     open override var trendingLinksLimit: Int { 0 }
     open override var trendingPostsLimit: Int { 0 }
