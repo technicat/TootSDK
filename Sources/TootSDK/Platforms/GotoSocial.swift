63bb69027214404592a7f0641871e0b2157e8f5b
Philip Chu <technicat@users.noreply.github.com>
2024-05-10 23:24:45 -0600

    gotosocial - remove featured tags, history, remove follower, translate, scheduled posts

--- Sources/TootSDK/Platforms/GotoSocial.swift
+++ Sources/TootSDK/Platforms/GotoSocial.swift
@@ -4,18 +4,31 @@ open class GotoSocial: MastoAPI {
 
     // supports
 
+    /// doesn't support featured tags
+    open override var supportsFeaturedTags: Bool { false }
     /// doesn't support deleting a notifitation
     open override var supportsNotificationDelete: Bool { false }
+    /// doesn't support post revision history
+    open override var supportsPostHistory: Bool { false }
     /// doesn't support public (no auth) timeline
     open override var supportsPublicTimeline: Bool { false }
+    /// doesn't support remove from followers
+    open override var supportsRemoveFollower: Bool { false }
     /// doesn't support oauth revoke revoke
     open override var supportsRevoke: Bool { false }
+    /// doesn't support post translations
+    open override var supportsTranslate: Bool { false }
+    
+    // limits
 
     /// conversations not supported
     open override var conversationsPageLimit: Int { 0 }
 
     /// muted accounts not supported
     open override var mutedAccountsLimit: Int { 0 }
+    
+    // scheduled posts not supported
+    open override var scheduledPageLimit: Int { 0 }
 
     /// trends not supported
     open override var trendingLinksLimit: Int { 0 }
