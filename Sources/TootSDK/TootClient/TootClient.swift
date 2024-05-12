9bae9260f1777b023838e35421ef6d782827ddef
Philip Chu <technicat@users.noreply.github.com>
2024-05-12 12:11:26 -0600

    cleanup

--- Sources/TootSDK/TootClient/TootClient.swift
+++ Sources/TootSDK/TootClient/TootClient.swift
@@ -331,15 +331,6 @@ extension TootClient {
 }
 
 extension TootClient {
-    /// Uses the currently available credentials to connect to an instance and detect the most compatible server flavour.
-    // @discardableResult
-    // public func connect() async throws -> Instance {
-    //     let instance = try await getInstance()
-    //     if debugInstance {
-    //         print("🎨 Detected fediverse instance flavour: \(instance.flavour), version: \(instance.version)")
-    //     }
-    //     return instance
-    // }
 
     /// Returns `true` if this instance of `TootClient` has no `accessToken`.
     public var isAnonymous: Bool {
