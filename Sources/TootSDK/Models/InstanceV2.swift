54d39d3f1d459cc1bb84ddead6c73f26d53e94ce
Philip Chu <technicat@users.noreply.github.com>
2024-05-13 21:16:41 -0600

    Update InstanceV2.swift

--- Sources/TootSDK/Models/InstanceV2.swift
+++ Sources/TootSDK/Models/InstanceV2.swift
@@ -1,65 +1,46 @@
 import Foundation
 
 /// General information about an instance
+/// https://docs.joinmastodon.org/entities/Instance/
+/// todo - fill in
 public struct InstanceV2: Codable, Hashable {
     public init(
-        uri: String? = nil,
-        title: String? = nil,
-        description: String? = nil,
-        shortDescription: String? = nil,
-        email: String? = nil,
+        domain: String,
+        title: String,
         version: String,
+        sourceUrl: String,
+        description: String,
         languages: [String]? = nil,
-        registrations: Bool? = nil,
-        approvalRequired: Bool? = nil,
-        invitesEnabled: Bool? = nil,
-        thumbnail: String? = nil,
         configuration: Configuration? = nil,
         contactAccount: Account? = nil,
         rules: [InstanceRule]? = nil
     ) {
-        self.uri = uri
+        self.domain = domain
         self.title = title
-        self.description = description
-        self.shortDescription = shortDescription
-        self.email = email
         self.version = version
+        self.sourceUrl = sourceUrl
+        self.description = description
         self.languages = languages
-        self.registrations = registrations
-        self.approvalRequired = approvalRequired
-        self.invitesEnabled = invitesEnabled
-        self.thumbnail = thumbnail
         self.configuration = configuration
         self.rules = rules
     }
-
+    
     /// The domain name of the instance.
-    public var uri: String?
+    public var domain: String
     /// The title of the website.
-    public var title: String?
-    /// Admin-defined description of the Fediverse site.
-    public var description: String?
-    /// A shorter description defined by the admin.
-    public var shortDescription: String?
-    /// An email that may be contacted for any inquiries.
-    public var email: String?
+    public var title: String
     /// The version of  the server installed on the instance.
     public var version: String
+    /// The URL for the source code of the software running on this instance, in keeping with AGPL license requirements..
+    public var sourceUrl: String
+    /// Admin-defined description of the Fediverse site.
+    public var description: String
     /// Primary languages of the website and its staff.
     public var languages: [String]?
-    /// Whether registrations are enabled.
-    public var registrations: Bool?
-    /// Whether registrations require moderator approval.
-    public var approvalRequired: Bool?
-    /// Whether invites are enabled.
-    public var invitesEnabled: Bool?
-    /// Banner image for the website.
-    public var thumbnail: String?
-    /// Configured values and limits for this instance.
     public var configuration: Configuration?
     /// An itemized list of rules for users of the instance.
     public var rules: [InstanceRule]?
-
+    
     public struct Configuration: Codable, Hashable {
         /// Limits related to accounts.
         public var accounts: Accounts?
@@ -69,19 +50,19 @@ public struct InstanceV2: Codable, Hashable {
         public var mediaAttachments: MediaAttachments?
         /// Limits related to polls.
         public var polls: Polls?
-
+        
         enum CodingKeys: String, CodingKey {
             case accounts
             case posts = "statuses"
             case mediaAttachments
             case polls
         }
-
+        
         public struct Accounts: Codable, Hashable {
             /// The maximum number of featured tags allowed for each account.
             public var maxFeaturedTags: Int?
         }
-
+        
         public struct Posts: Codable, Hashable {
             /// The maximum number of allowed characters per post.
             public var maxCharacters: Int?
@@ -90,7 +71,7 @@ public struct InstanceV2: Codable, Hashable {
             /// Each URL in a post will be assumed to be exactly this many characters.
             public var charactersReservedPerUrl: Int?
         }
-
+        
         public struct MediaAttachments: Codable, Hashable {
             /// Contains MIME types that can be uploaded.
             public var supportedMimeTypes: [String]?
@@ -105,7 +86,7 @@ public struct InstanceV2: Codable, Hashable {
             /// The maximum number of pixels (width times height) for video uploads.
             public var videoMatrixLimit: Int?
         }
-
+        
         public struct Polls: Codable, Hashable {
             /// Each poll is allowed to have up to this many options.
             public var maxOptions: Int?
@@ -116,23 +97,6 @@ public struct InstanceV2: Codable, Hashable {
             /// The longest allowed poll duration, in seconds.
             public var maxExpiration: Int?
         }
-
-    }
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.uri = try? container.decodeIfPresent(String.self, forKey: .uri)
-        self.title = try? container.decodeIfPresent(String.self, forKey: .title)
-        self.description = try? container.decodeIfPresent(String.self, forKey: .description)
-        self.shortDescription = try? container.decodeIfPresent(String.self, forKey: .shortDescription)
-        self.email = try? container.decodeIfPresent(String.self, forKey: .email)
-        self.version = try container.decode(String.self, forKey: .version)
-        self.languages = try? container.decodeIfPresent([String].self, forKey: .languages)
-        self.registrations = try? container.decodeIfPresent(Bool.self, forKey: .registrations)
-        self.approvalRequired = try? container.decodeIfPresent(Bool.self, forKey: .approvalRequired)
-        self.invitesEnabled = try? container.decodeIfPresent(Bool.self, forKey: .invitesEnabled)
-        self.thumbnail = try? container.decodeIfPresent(String.self, forKey: .thumbnail)
-        self.configuration = try? container.decodeIfPresent(Configuration.self, forKey: .configuration)
-        self.rules = try? container.decodeIfPresent([InstanceRule].self, forKey: .rules)
+        
     }
 }
