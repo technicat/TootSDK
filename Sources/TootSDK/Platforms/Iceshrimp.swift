c02f572cba3bc6891f0e2d112c6ba7cea039a8b2
Philip Chu <technicat@users.noreply.github.com>
2024-05-14 17:25:23 -0600

    iceshrimp (and catodon) don't support instance v2

--- Sources/TootSDK/Platforms/Iceshrimp.swift
+++ Sources/TootSDK/Platforms/Iceshrimp.swift
@@ -7,6 +7,7 @@ open class Iceshrimp: Firefish {
 
     open override var supportsFilter: Bool { false }
     open override var supportsInstanceExtendedDescription: Bool { false }
+    open override var supportsInstanceV2: Bool { false }
     open override var supportsIsBoosted: Bool { true }
     open override var supportsPostDeleteAndEdit: Bool { true }
     open override var supportsReportRules: Bool { false }
