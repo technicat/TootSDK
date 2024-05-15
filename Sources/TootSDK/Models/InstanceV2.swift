b5a15ac7d39b92d63d9baefc0439de6001ce42e9
Philip Chu <technicat@users.noreply.github.com>
2024-05-14 23:59:01 -0600

    rm blurhash for now

--- Sources/TootSDK/Models/InstanceV2.swift
+++ Sources/TootSDK/Models/InstanceV2.swift
@@ -50,6 +50,6 @@ public struct InstanceV2: Codable, Hashable {
         /// The URL for the thumbnail image.
         public var url: String
         /// A hash computed by the BlurHash algorithm, for generating colorful preview thumbnails when media has not been downloaded yet.
-        public var blurhash: String?
+      //  public var blurhash: String? // causing crashes?
     }
 }
