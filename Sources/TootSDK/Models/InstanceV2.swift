f4ac8eb1c83cdd8d900892400239117928dd5a62
Philip Chu <technicat@users.noreply.github.com>
2024-05-17 16:38:46 -0600

    add usage to instance v2

--- Sources/TootSDK/Models/InstanceV2.swift
+++ Sources/TootSDK/Models/InstanceV2.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 /// General information about an instance
+/// fields are listed in order of the spec
 /// https://docs.joinmastodon.org/entities/Instance/
 /// todo - fill in
 public struct InstanceV2: Codable, Hashable {
@@ -10,10 +11,11 @@ public struct InstanceV2: Codable, Hashable {
         version: String,
         sourceUrl: String,
         description: String? = nil,
+        usage: Usage,
+        thumbnail: Thumbnail? = nil,
         languages: [String]? = nil,
         configuration: Configuration,
         rules: [InstanceRule]? = nil,
-        thumbnail: Thumbnail? = nil,
         registrations: Registrations,
         contact: Contact
     ) {
@@ -22,12 +24,13 @@ public struct InstanceV2: Codable, Hashable {
         self.version = version
         self.sourceUrl = sourceUrl
         self.description = description
+        self.usage = usage
+        self.thumbnail = thumbnail
         self.languages = languages
         self.configuration = configuration
-        self.rules = rules
-        self.thumbnail = thumbnail
         self.registrations = registrations
         self.contact = contact
+        self.rules = rules
     }
 
     /// The domain name of the instance.
@@ -40,20 +43,23 @@ public struct InstanceV2: Codable, Hashable {
     public var sourceUrl: String
     /// Admin-defined description of the Fediverse site.
     public var description: String?
+    /// Usage data for this instance.
+    public var usage: Usage
+    // todo - add usage
+    /// An image used to represent this instance.
+    /// Not optional in spec but missing in Mitra
+    public var thumbnail: Thumbnail?
     /// Primary languages of the website and its staff.
     public var languages: [String]?
     /// Configured values and limits for this website.
     /// Not optional in spec but consistent with V1
     public var configuration: Configuration?
-    /// An itemized list of rules for users of the instance.
-    public var rules: [InstanceRule]?
-    /// An image used to represent this instance.
-    /// Not optional in spec but missing in Mitra
-    public var thumbnail: Thumbnail?
     /// Information about registering for this website.
     public var registrations: Registrations
     /// Hints related to contacting a representative of the website.
     public var contact: Contact
+    /// An itemized list of rules for users of the instance.
+    public var rules: [InstanceRule]?
 
     public struct Thumbnail: Codable, Hashable {
         /// The URL for the thumbnail image.
@@ -77,5 +83,17 @@ public struct InstanceV2: Codable, Hashable {
         public var email: String
         /// An account that can be contacted natively over the network regarding inquiries or issues.
         public var account: Account?  // causing crashes?
+    }
+    
+    public struct Usage: Codable, Hashable {
+        /// Usage data related to users on this instance.
+        public var users: Users
+        
+    }
+    
+    public struct Users: Codable, Hashable {
+        /// The number of active users in the past 4 weeks.
+        public var activeMonth: Int
+        
     }
 }
