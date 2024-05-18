bb77448add2e566baa94498f287f0e28550e5a24
Philip Chu <technicat@users.noreply.github.com>
2024-05-18 10:30:06 -0600

    Account.highlightedRole

--- Sources/TootSDK/Extensions/Account/AccountRole.swift
+++ Sources/TootSDK/Extensions/Account/AccountRole.swift
@@ -3,4 +3,15 @@ extension Account {
     public var hasRole: Bool {
         role != nil
     }
+        
+    public var highlightedRole: TootRole? {
+        guard let role else {
+            return nil
+        }
+        guard role.highlighted else {
+            return nil
+        }
+        return role
+    }
+
 }
