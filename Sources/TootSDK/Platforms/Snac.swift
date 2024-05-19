ef21c3abee2bd30ce0eede5b741fa2c3e165428c
Philip Chu <technicat@users.noreply.github.com>
2024-05-19 13:39:46 -0600

    snac doesn't support instance v2

--- Sources/TootSDK/Platforms/Snac.swift
+++ Sources/TootSDK/Platforms/Snac.swift
@@ -1,5 +1,7 @@
 open class Snac: MastoAPI {
     open override var name: String { "Snac" }
+    
     open override var supportsInstanceExtendedDescription: Bool { false }
+    open override var supportsInstanceV2: Bool { false }
     open override var supportsTranslate: Bool { false }
 }
