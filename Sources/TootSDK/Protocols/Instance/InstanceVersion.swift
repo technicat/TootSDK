dac7cd1787ac7e8686f98b3cd8f42f00adb70680
Philip Chu <technicat@users.noreply.github.com>
2024-05-19 13:17:22 -0600

    api version returns remainder of version string

--- Sources/TootSDK/Protocols/Instance/InstanceVersion.swift
+++ Sources/TootSDK/Protocols/Instance/InstanceVersion.swift
@@ -4,20 +4,21 @@ import RegexBuilder
 // e.g. hometown uses + instead of () to separate from the API version
 extension Instance {
 
-    public var apiVersion: Version {
+    public var platformVersion: (api: Version, platform: String) {
         let regex = Regex {
             Capture(OneOrMore(.digit))
             "."
             Capture(OneOrMore(.digit))
             "."
             Capture(OneOrMore(.digit))
+            Capture(ZeroOrMore(.any))
         }
         if let match = version.firstMatch(of: regex) {
-            return Version(Int(match.1) ?? 0, Int(match.2) ?? 0, Int(match.3) ?? 0)
+            return (Version(Int(match.1) ?? 0, Int(match.2) ?? 0, Int(match.3) ?? 0), String(match.4))
         } else {
             // if we can't find the version, make up something big
             // give all features a chance
-            return Version(100, 0)
+            return (Version(100, 0), "")
         }
     }
 }
