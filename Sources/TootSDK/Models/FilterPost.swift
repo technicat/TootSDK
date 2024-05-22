05158a337cf3239e8d3a9e90c729f8916c5ffec0
Philip Chu <technicat@users.noreply.github.com>
2024-05-21 22:38:17 -0600

    status_id -> postId

--- Sources/TootSDK/Models/FilterPost.swift
+++ Sources/TootSDK/Models/FilterPost.swift
@@ -3,10 +3,16 @@
 
 import Foundation
 
+/// https://docs.joinmastodon.org/entities/FilterStatus/
+/// Represents a status ID that, if matched, should cause the filter action to be taken.
 public struct FilterPost: Codable, Hashable, Identifiable {
     /// The ID of the FilterStatus in the database.
     public var id: String
-    // swift-format-ignore: AlwaysUseLowerCamelCase
     /// The ID of the Post that will be filtered.
-    public var status_id: String
+    public var postId: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case postId = "status_id"
+    }
 }
