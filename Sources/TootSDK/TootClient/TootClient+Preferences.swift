381dc016d39360e841c1b3e2399b0b614f07be60
Philip Chu <technicat@users.noreply.github.com>
2024-05-22 20:34:50 -0600

    fix missing extension wrapper

--- Sources/TootSDK/TootClient/TootClient+Preferences.swift
+++ Sources/TootSDK/TootClient/TootClient+Preferences.swift
@@ -7,12 +7,15 @@
 
 import Foundation
 
-/// Preferences defined by the user in their account settings.
-/// - Returns: Preferences by key and value
-public func getPreferences() async throws -> Preferences {
-    let req = HTTPRequestBuilder {
-        $0.url = getURL(["api", "v1", "preferences"])
-        $0.method = .get
+extension TootClient {
+    
+    /// Preferences defined by the user in their account settings.
+    /// - Returns: Preferences by key and value
+    public func getPreferences() async throws -> Preferences {
+        let req = HTTPRequestBuilder {
+            $0.url = getURL(["api", "v1", "preferences"])
+            $0.method = .get
+        }
+        return try await fetch(Preferences.self, req)
     }
-    return try await fetch(Preferences.self, req)
 }
