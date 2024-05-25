230172223c06528e69010d1622c671327d793502
Philip Chu <technicat@users.noreply.github.com>
2024-05-24 23:28:09 -0600

    Create PreferencesTests.swift

---
+++ Tests/TootSDKTests/PreferencesTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+
+@testable import TootSDK
+
+final class PreferencesTests: XCTestCase {
+   
+    func test() throws {
+        // arrange
+        let json = localContent("preferences")
+        let decoder = TootDecoder()
+
+        // act
+        let result = try decoder.decode(Preferences.self, from: json)
+
+        // assert
+        XCTAssertNotNil(result)
+    }
+    
+}
