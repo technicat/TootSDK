2aec99baf64da0e58e3429d63c7c23a4d89ae122
Philip Chu <technicat@users.noreply.github.com>
2024-05-25 20:34:45 -0600

    Update AdminAccount.swift

--- Sources/TootSDK/Models/AdminAccount.swift
+++ Sources/TootSDK/Models/AdminAccount.swift
@@ -7,15 +7,38 @@
 
 import Foundation
 
+/// https://docs.joinmastodon.org/entities/Admin_Account/
 public struct AdminAccount: Codable, Hashable, Identifiable {
     public init(
         id: String,
+        username: String,
+        domain: String? = nil,
+        createdAt: Date,
+        email: String? = nil,
+        ip: String? = nil,
         account: Account
     ) {
         self.id = id
+        self.username = username
+        self.domain = domain
+        self.createdAt = createdAt
+        self.email = email
+        self.ip = ip
         self.account = account
     }
 
     public var id: String
+    /// The username of the account.
+    public let username: String
+    /// The domain of the account, if it is remote.
+    public let domain: String?
+    /// When the account was first discovered.
+    public let createdAt: Date
+    /// The email address associated with the account.
+    /// spec says non-null but actually sometimes is explicitly null
+    public let email: String?
+    /// The IP address last used to login to this account.
+    public let ip: String?
+    /// User-level information about the account.
     public var account: Account
 }
