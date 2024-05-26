4f745a01e3e153e3ed6b8e9070f37f7bf20499cb
Philip Chu <technicat@users.noreply.github.com>
2024-05-26 00:51:39 -0600

    Create AdminAccountV2Params.swift

---
+++ Sources/TootSDK/Models/Admin/AdminAccountV2Params.swift
@@ -0,0 +1,70 @@
+//  AdminAccountParams.swift
+//  Created by Phil Chu on 05/25/2024.
+
+import Foundation
+
+/// https://docs.joinmastodon.org/methods/admin/accounts/#v2
+public struct AdminAccountV2Params: Sendable {
+    /// Filter for local or remote accounts.
+    public var origin: String?
+    /// Filter for active, pending, disabled, silenced, or suspended accounts.
+    /// todo - use enums
+    public var status: String?
+    /// Filter for accounts with staff permissions (users that can manage reports).
+    public var permissions: String?
+    /// Filter for users with these roles..
+    public var roleIds: [String]?
+    /// Lookup users invited by the account with this ID.
+    public var invitedBy: String?
+    /// Search for the given username.
+    public var username: String?
+    /// Search for the given display name.
+    public var displayName: String?
+    /// Filter by the given domain.
+    public var byDomain: String?
+    /// Lookup a user with this email.
+    public var email: String?
+    /// Lookup users with this IP address.
+    public var ip: String?
+
+    public init(
+        origin: String? = nil,
+        status: String? = nil,
+        permissions: String? = nil,
+        roleIds: [String]? = nil,
+        invitedBy: String? = nil,
+        username: String? = nil,
+        displayName: String? = nil,
+        byDomain: String? = nil,
+        email: String? = nil,
+        ip: String? = nil
+    ) {
+        self.origin = origin
+        self.status = status
+        self.permissions = permissions
+        self.roleIds = roleIds
+        self.invitedBy = invitedBy
+        self.username = username
+        self.displayName = displayName
+        self.byDomain = byDomain
+        self.email = email
+        self.ip = ip
+    }
+}
+
+extension AdminAccountV2Params {
+    var queryItems: [URLQueryItem] {
+        [
+            URLQueryItem(name: "origin", value: origin),
+            URLQueryItem(name: "status", value: status),
+            URLQueryItem(name: "permissions", value: permissions),
+            URLQueryItem(name: "invited_by", value: invitedBy),
+            URLQueryItem(name: "username", value: username),
+            URLQueryItem(name: "display_name", value: displayName),
+            URLQueryItem(name: "by_domain", value: byDomain),
+            URLQueryItem(name: "email", value: email),
+            URLQueryItem(name: "ip", value: ip),
+           
+        ].filter { $0.value != nil }
+    }
+}
