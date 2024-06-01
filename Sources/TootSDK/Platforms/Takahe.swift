7363eb63c18d061355c599f5f13e7bcdd09152a6
Philip Chu <technicat@users.noreply.github.com>
2024-05-31 19:29:55 -0600

    comment

--- Sources/TootSDK/Platforms/Takahe.swift
+++ Sources/TootSDK/Platforms/Takahe.swift
@@ -3,6 +3,7 @@ open class Takahe: MastoCompatible {
     open override var name: String { "Takahe" }
 
     public init(_ api: Version) {
+        // arbitrary mastodon api
         super.init(Version(3, 5), api)
     }
 }
