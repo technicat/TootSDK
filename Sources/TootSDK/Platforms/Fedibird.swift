93a2bd2982196f82de0508a4a4c034e891340be3
Philip Chu <technicat@users.noreply.github.com>
2024-05-31 19:31:17 -0600

    hardcode fedibird version

--- Sources/TootSDK/Platforms/Fedibird.swift
+++ Sources/TootSDK/Platforms/Fedibird.swift
@@ -1,5 +1,14 @@
 open class Fedibird: Mastodon {
 
     open override var name: String { "Fedibird" }
+    
+    public init() {
+        // hardcode the mastodon api
+        // it's not in the nodeinfo
+        // it's in the instance version string
+        // but doesn't list the platform name
+        // it's not getting updated, anyway
+        super.init(Version(3,4,1))
+    }
 
 }
