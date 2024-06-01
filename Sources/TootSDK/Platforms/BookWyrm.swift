e0d323f0edce0486f062df73e603674eaff022c3
Philip Chu <technicat@users.noreply.github.com>
2024-06-01 17:21:50 -0600

    make bookwyrm mastodon api non-zero (avoid getting overridden)

--- Sources/TootSDK/Platforms/BookWyrm.swift
+++ Sources/TootSDK/Platforms/BookWyrm.swift
@@ -1,10 +1,11 @@
 open class BookWyrm: MastoCompatible {
 
     open override var name: String { "BookWyrm" }
-
+    
     public init(_ build: Version) {
-        super.init(Version(), build)
-    }
+        // non-zero so it doesn't get overriden
+            super.init(Version(0,0,1), build)
+        }
 
     // only supports instanceV1 and peers
 
