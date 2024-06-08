48420e8915e556bc9fc0bad3860c9c43fdb94129
Philip Chu <technicat@users.noreply.github.com>
2024-06-07 19:35:25 -0600

    Update AdminAccountRefresh.swift

--- Sources/TootSDK/TootExt/AdminAccount/AdminAccountRefresh.swift
+++ Sources/TootSDK/TootExt/AdminAccount/AdminAccountRefresh.swift
@@ -1,7 +1,7 @@
 extension TootClient {
     
     /// https://docs.joinmastodon.org/methods/admin/accounts/#get-one
-    public func refresh(_ account: AdminAccount) async throws -> Account {
+    public func refresh(_ account: AdminAccount) async throws -> AdminAccount {
         try await getAdminAccount(by: account.id)
     }
 }
