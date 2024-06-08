b51ac0d4ff64899b1939764c779bd8c4c28ada3e
Philip Chu <technicat@users.noreply.github.com>
2024-06-07 23:46:19 -0600

    more admin account functions

--- Sources/TootSDK/TootClient/TootClient+Admin+Account.swift
+++ Sources/TootSDK/TootClient/TootClient+Admin+Account.swift
@@ -71,4 +71,76 @@ extension TootClient {
         }
         return try await fetch(AdminAccount.self, req)
     }
+    
+    /// Reject the given local account if it is currently pending approval.
+    /// - Parameter id: the ID of the Account in the instance database.
+    /// - Returns: the account requested, or an error if unable to retrieve
+    /// https://docs.joinmastodon.org/methods/admin/accounts/#reject
+    public func rejectAccount(by id: String) async throws -> AdminAccount {
+        let req = HTTPRequestBuilder {
+            $0.url = getURL(["api", "v1", "admin", "accounts", id, "reject"])
+            $0.method = .post
+        }
+        return try await fetch(AdminAccount.self, req)
+    }
+    
+    /// Permanently delete data for a suspended account.
+    /// - Parameter id: the ID of the Account in the instance database.
+    /// - Returns: the account requested, or an error if unable to retrieve
+    /// https://docs.joinmastodon.org/methods/admin/accounts/#delete
+    public func deleteAccount(by id: String) async throws -> AdminAccount {
+        let req = HTTPRequestBuilder {
+            $0.url = getURL(["api", "v1", "admin", "accounts", id])
+            $0.method = .delete
+        }
+        return try await fetch(AdminAccount.self, req)
+    }
+    
+    /// Re-enable a local account whose login is currently disabled.
+    /// - Parameter id: the ID of the Account in the instance database.
+    /// - Returns: the account requested, or an error if unable to retrieve
+    /// https://docs.joinmastodon.org/methods/admin/accounts/#enable
+    public func enableAccount(by id: String) async throws -> AdminAccount {
+        let req = HTTPRequestBuilder {
+            $0.url = getURL(["api", "v1", "admin", "accounts", id, "enable"])
+            $0.method = .post
+        }
+        return try await fetch(AdminAccount.self, req)
+    }
+    
+    /// Unsilence an account if it is currently silenced.
+    /// - Parameter id: the ID of the Account in the instance database.
+    /// - Returns: the account requested, or an error if unable to retrieve
+    /// https://docs.joinmastodon.org/methods/admin/accounts/#unsilence
+    public func unsilenceAccount(by id: String) async throws -> AdminAccount {
+        let req = HTTPRequestBuilder {
+            $0.url = getURL(["api", "v1", "admin", "accounts", id, "unsilence"])
+            $0.method = .post
+        }
+        return try await fetch(AdminAccount.self, req)
+    }
+    
+    /// Unsuspend an account if it is currently suspended.
+    /// - Parameter id: the ID of the Account in the instance database.
+    /// - Returns: the account requested, or an error if unable to retrieve
+    /// https://docs.joinmastodon.org/methods/admin/accounts/#unsuspend
+    public func unsuspendAccount(by id: String) async throws -> AdminAccount {
+        let req = HTTPRequestBuilder {
+            $0.url = getURL(["api", "v1", "admin", "accounts", id, "unsuspend"])
+            $0.method = .post
+        }
+        return try await fetch(AdminAccount.self, req)
+    }
+    
+    /// Stops marking an account’s posts as sensitive, if it was previously flagged as sensitive.
+    /// - Parameter id: the ID of the Account in the instance database.
+    /// - Returns: the account requested, or an error if unable to retrieve
+    /// https://docs.joinmastodon.org/methods/admin/accounts/#unsensitive
+    public func unsensitiveAccount(by id: String) async throws -> AdminAccount {
+        let req = HTTPRequestBuilder {
+            $0.url = getURL(["api", "v1", "admin", "accounts", id, "unsensitive"])
+            $0.method = .post
+        }
+        return try await fetch(AdminAccount.self, req)
+    }
 }
