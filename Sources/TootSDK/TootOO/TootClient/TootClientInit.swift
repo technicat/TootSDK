053aab4cb2b575c05c2c6e2a475b4cf688879f7e
Philip Chu <technicat@users.noreply.github.com>
2024-06-08 13:22:01 -0600

    make lint

--- Sources/TootSDK/TootOO/TootClient/TootClientInit.swift
+++ Sources/TootSDK/TootOO/TootClient/TootClientInit.swift
@@ -1,38 +1,41 @@
 import Foundation
 
+/// https://docs.joinmastodon.org/api/oauth-scopes/
 public enum OAuthScope: String {
-  case read
-  case write
-  case follow
-  case adminRead = "admin:read"
+    case read
+    case write
+    case follow
+    case push
+    case adminRead = "admin:read"
     case adminWrite = "admin:write"
+    // todo - add granular scopes
 }
 
 extension TootClient {
 
-  /// for login
-  convenience init(
-    _ url: URL,
-    client: String,
-    scopes: [OAuthScope],
-    log: Bool = false
-  ) throws {
-    self.init(
-      clientName: client,
-      instanceURL: url,
-      scopes: scopes.map { $0.rawValue })
-    self.debug = log
-  }
+    /// for login
+    public convenience init(
+        _ url: URL,
+        client: String,
+        scopes: [OAuthScope] = [],
+        log: Bool = false
+    ) throws {
+        self.init(
+            clientName: client,
+            instanceURL: url,
+            scopes: scopes.map { $0.rawValue })
+        self.debug = log
+    }
 
-  /// to resume session
-  convenience init(
-    _ url: URL,
-    client: String,
-    token: String
-  ) throws {
-    self.init(
-      clientName: client,
-      instanceURL: url,
-      accessToken: token)
-  }
+    /// to resume session
+    public convenience init(
+        _ url: URL,
+        client: String,
+        token: String
+    ) throws {
+        self.init(
+            clientName: client,
+            instanceURL: url,
+            accessToken: token)
+    }
 }
