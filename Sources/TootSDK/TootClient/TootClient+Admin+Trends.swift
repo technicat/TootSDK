d558fd5f61ff43f588004a8c1bd85b0557893797
Philip Chu <technicat@users.noreply.github.com>
2024-06-16 14:17:14 -0600

    getAdminTrendingPosts

--- Sources/TootSDK/TootClient/TootClient+Admin+Trends.swift
+++ Sources/TootSDK/TootClient/TootClient+Admin+Trends.swift
@@ -12,6 +12,7 @@ extension TootClient {
     /// Links that have been shared more than others, including unapproved and unreviewed links.
     ///
     /// - Returns: Array of ``TrendingLink``.
+    /// https://docs.joinmastodon.org/methods/admin/trends/#links
     public func getAdminTrendingLinks() async throws -> [TrendingLink] {
         let req = HTTPRequestBuilder {
             $0.url = getURL(["api", "v1", "admin", "trends", "links"])
@@ -19,5 +20,18 @@ extension TootClient {
         }
         
         return try await fetch([TrendingLink].self, req)
+    }
+    
+    /// Posts that have been shared more than others, including unapproved and unreviewed posts.
+    ///
+    /// - Returns: Array of ``Post``.
+    /// https://docs.joinmastodon.org/methods/admin/trends/#statuses
+    public func getAdminTrendingPosts() async throws -> [Post] {
+        let req = HTTPRequestBuilder {
+            $0.url = getURL(["api", "v1", "admin", "trends", "statuses"])
+            $0.method = .get
+        }
+        
+        return try await fetch([Post].self, req)
     }
 }
