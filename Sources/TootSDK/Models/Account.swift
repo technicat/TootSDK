7d7a5a4b737401d909b1efcf9e4007336a409e9a
Philip Chu <technicat@users.noreply.github.com>
2024-06-17 19:54:20 -0600

    mv CredentialAccount to a new file

--- Sources/TootSDK/Models/Account.swift
+++ Sources/TootSDK/Models/Account.swift
@@ -176,24 +176,3 @@ extension Account: CustomDebugStringConvertible {
         return "Account with id: \(id)"
     }
 }
-
-// https://docs.joinmastodon.org/entities/Account/#CredentialAccount
-/// An extra entity to be used with API methods to verify credentials and update credentials
-public class CredentialAccount: Account {
-
-    public let source: TootSource?
-    /// The role assigned to the currently authorized user.
-    public let role: TootRole?
-    
-    public enum CodingKeys: String, CodingKey {
-        case source
-        case role
-    }
-    
-    required public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.source = try? container.decodeIfPresent(TootSource.self, forKey: .source)
-        self.role = try? container.decodeIfPresent(TootRole.self, forKey: .role)
-        try super.init(from: decoder)
-    }
-}
---
+++ Sources/TootSDK/Models/CredentialAccount.swift
@@ -0,0 +1,29 @@
+//
+//  CredentialAccount.swift
+//  
+//
+//  Created by Philip Chu on 6/17/24.
+//
+
+import Foundation
+
+// https://docs.joinmastodon.org/entities/Account/#CredentialAccount
+/// An extra entity to be used with API methods to verify credentials and update credentials
+public class CredentialAccount: Account {
+
+    public let source: TootSource?
+    /// The role assigned to the currently authorized user.
+    public let role: TootRole?
+    
+    public enum CodingKeys: String, CodingKey {
+        case source
+        case role
+    }
+    
+    required public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.source = try? container.decodeIfPresent(TootSource.self, forKey: .source)
+        self.role = try? container.decodeIfPresent(TootRole.self, forKey: .role)
+        try super.init(from: decoder)
+    }
+}
