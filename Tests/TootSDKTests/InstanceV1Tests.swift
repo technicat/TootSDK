69b527201a6def4e7d618413b575feb47ee22bd7
Philip Chu <technicat@users.noreply.github.com>
2024-06-21 10:58:21 -0600

    test bookwyrm instance

--- Tests/TootSDKTests/InstanceV1Tests.swift
+++ Tests/TootSDKTests/InstanceV1Tests.swift
@@ -129,4 +129,16 @@ final class InstanceV1Tests: XCTestCase {
         // assert
         XCTAssertNotNil(result)
     }
+
+    func testBookwyrm() throws {
+        // arrange
+        let json = localContent("instancev1_bookwyrm")
+        let decoder = TootDecoder()
+
+        // act
+        let result = try decoder.decode(InstanceV1.self, from: json)
+
+        // assert
+        XCTAssertNotNil(result)
+    }
 }
