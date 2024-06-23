ae67c9dd70d5ff9f3440e8c2e67c84c174ebb228
Philip Chu <technicat@users.noreply.github.com>
2024-06-23 15:08:59 -0600

    lint

--- Sources/TootSDK/Models/TootGrantType.swift
+++ Sources/TootSDK/Models/TootGrantType.swift
@@ -11,6 +11,6 @@ import Foundation
 public enum TootGrantType: String {
     case login = "authorization_code"
     case register = "client_credentials"
-// https://docs-develop.pleroma.social/backend/development/API/differences_in_mastoapi_responses/
+    // https://docs-develop.pleroma.social/backend/development/API/differences_in_mastoapi_responses/
     case refreshToken = "refresh_token"
 }
