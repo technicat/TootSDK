ccf3cf62c95b4a2eb8f7fb9d0603403555c642c9
Philip Chu <technicat@users.noreply.github.com>
2024-06-23 23:09:44 -0600

    Update PleromaInstance.swift

--- Sources/TootSDK/Models/Pleroma/PleromaInstance.swift
+++ Sources/TootSDK/Models/Pleroma/PleromaInstance.swift
@@ -8,4 +8,17 @@
 import Foundation
 
 public struct PleromaInstance: Codable, Hashable {
+
+    public var vapidPublicKey: String
+
+    public var stats: Stats
+    public var metadata: Metadata
+
+    public struct Stats: Codable, Hashable {
+        public var mau: Int
+    }
+    
+    public struct Metadata: Codable, Hashable {
+    // todo fill in
+    }
 }
