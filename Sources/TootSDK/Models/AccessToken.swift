ac8554375839c1856e84cb3f7f79a595ddade03c
Philip Chu <technicat@users.noreply.github.com>
2024-06-24 16:50:28 -0600

    add date to access token

--- Sources/TootSDK/Models/AccessToken.swift
+++ Sources/TootSDK/Models/AccessToken.swift
@@ -3,8 +3,25 @@
 
 import Foundation
 
+/// https://docs.joinmastodon.org/entities/Token/
 public struct AccessToken: Codable {
     public let scope: String?
     public let tokenType: String?
     public let accessToken: String?
+    public let createdAt: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case scope
+        case tokenType
+        case accessToken
+        case createdAt
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.scope = try container.decodeIfPresent(String.self, forKey: .scope)
+        self.tokenType = try container.decodeIfPresent(String.self, forKey: .tokenType)
+        self.accessToken = try container.decodeIfPresent(String.self, forKey: .accessToken)
+        self.createdAt = try container.decodeUnixDate(forKey: .createdAt)
+    }
 }
