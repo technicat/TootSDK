fecbef8f8b368e7904276fa813e3ed18c651c971
Philip Chu <technicat@users.noreply.github.com>
2024-06-29 00:32:02 -0600

    ISOCode two-letter codes

--- Sources/TootSDK/Platforms/Friendica.swift
+++ Sources/TootSDK/Platforms/Friendica.swift
@@ -17,36 +17,7 @@ open class Friendica: MastoAPI {
 
     // lists
     // all the two-letter codes
-    open override var languages: ISOCodes {
-        [
-            .aa, .ab, .ae, .af, .ak, .am, .an, .ar, .as, .av, .ay, .az,
-            .ba, .be, .bg, .bh, .bi, .bm, .bn, .bo, .br, .bs,
-            .ca, .ce, .ch, .co, .cr, .cs, .cu, .cv, .cy,
-            .da, .de, .dv, .dz,
-            .ee, .el, .en, .eo, .es, .et, .eu,
-            .fa, .fi, .ff, .fj, .fo, .fr, .fy,
-            .ga, .gd, .gl, .gn, .gu, .gv,
-            .ha, .he, .hi, .ho, .hr, .hu, .ht, .hy, .hz,
-            .ia, .id, .ie, .ig, .ii, .ik, .io, .is, .it, .iu,
-            .ja, .jv,
-            .ka, .kg, .ki, .kj, .kk, .kl, .km, .kn, .ko, .kr, .ks, .ku, .kv, .kw, .ky,
-            .la, .lb, .lg, .li, .ln, .lo, .lt, .lu, .lv,
-            .mg, .mh, .mi, .mk, .ml, .mn, .mr, .ms, .mt, .my,
-            .na, .nb, .nd, .ne, .ng, .nl, .nn, .no, .nr, .nv, .ny,
-            .oc, .oj, .om, .or, .os,
-            .pa, .pi, .pl, .ps, .pt,
-            .qu,
-            .rm, .rn, .ro, .ru, .rw,
-            .sa, .sc, .sd, .se, .sg, .si, .sk, .sl, .sm, .sn, .so, .sq, .sr, .ss, .st, .su, .sv, .sw,
-            .ta, .te, .tg, .th, .ti, .tl, .tk, .tn, .to, .tr, .ts, .tt, .tw, .ty,
-            .ug, .uk, .ur, .uz,
-            .ve, .vi, .vo,
-            .wa, .wo,
-            .xh,
-            .yi, .yo,
-            .za, .zh, .zu,
-        ]
-    }
+    open override var languages: ISOCodes { ISOCode.ISO639_1 }
 
     open override var notificationTypes: NotificationTypes {
         // setting this to empty (unsupported)
