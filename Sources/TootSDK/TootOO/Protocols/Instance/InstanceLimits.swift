69f1c9b3de8124f7f314d0cba059558ec47b504e
Philip Chu <technicat@users.noreply.github.com>
2024-06-30 09:23:18 -0600

    instance poll limits

--- Sources/TootSDK/TootOO/Protocols/Instance/InstanceLimits.swift
+++ Sources/TootSDK/TootOO/Protocols/Instance/InstanceLimits.swift
@@ -23,4 +23,23 @@ extension Instance {
     public var maxPins: Int? {
         configuration?.accounts?.maxPinnedStatuses
     }
+    
+    // polls
+    
+    public var maxPollOptions: Int? {
+        configuration?.polls?.maxOptions
+    }
+    
+    public var maxPollChars: Int? {
+        configuration?.polls?.maxCharactersPerOption
+    }
+    
+    public var minPollExpiration: Int? {
+        configuration?.polls?.minExpiration
+    }
+    
+    public var maxPollExpiration: Int? {
+        configuration?.polls?.maxExpiration
+    }
+    
 }
