65637e06f93aa4eff63c18dc69156937d9ff4e75
Philip Chu <technicat@users.noreply.github.com>
2024-07-04 01:29:28 -0600

    isHiding predicates

--- Sources/TootSDK/TootOO/Extensions/Account/AccountHideCollections.swift
+++ Sources/TootSDK/TootOO/Extensions/Account/AccountHideCollections.swift
@@ -7,3 +7,26 @@ extension CredentialAccount {
     }
 
 }
+
+extension Account {
+    
+    public var isHidingFollows: Bool {
+        hideCollections ?? pleroma?.hideFollows ?? false
+    }
+    
+    public var isHidingFollowers: Bool {
+        hideCollections ?? pleroma?.hideFollowers ?? false
+    }
+    
+    public var isHidingFollowerCount: Bool {
+        hideCollections ?? pleroma?.hideFollowersCount ?? false
+    }
+    
+    public var isHidingFollowCount: Bool {
+        hideCollections ?? pleroma?.hideFollowsCount ?? false
+    }
+    
+    public var isHidingFaves: Bool {
+        pleroma?.hideFavorites ?? false
+    }
+}
