7eec0139d98766399a6585ca212e1f9ce769a628
Philip Chu <technicat@users.noreply.github.com>
2024-07-04 20:43:03 -0600

    format

--- Sources/TootSDK/TootOO/Extensions/Account/AccountHideCollections.swift
+++ Sources/TootSDK/TootOO/Extensions/Account/AccountHideCollections.swift
@@ -3,7 +3,7 @@ extension CredentialAccount {
     // ideally check on CredentialAccount source which works on 4.2
     // otherwise fallback to plain account which works on 4.3
     public var isHidingCollections: Bool {
-        source?.hideCollections ?? false 
+        source?.hideCollections ?? false
         // ?? hideCollections ?? false
     }
 
@@ -11,33 +11,31 @@ extension CredentialAccount {
 
 // should rethink how the precedence works here
 extension Account {
-    
+
     public var isHidingFollowLists: Bool {
-        isHidingFollows &&
-        isHidingFollowers
+        isHidingFollows && isHidingFollowers
     }
-    
+
     public var isHidingFollowCounts: Bool {
-        isHidingFollowerCount &&
-        isHidingFollowCount
+        isHidingFollowerCount && isHidingFollowCount
     }
-    
+
     public var isHidingFollows: Bool {
         hideCollections ?? pleroma?.hideFollows ?? false
     }
-    
+
     public var isHidingFollowers: Bool {
         hideCollections ?? pleroma?.hideFollowers ?? false
     }
-    
+
     public var isHidingFollowerCount: Bool {
         hideCollections ?? pleroma?.hideFollowersCount ?? false
     }
-    
+
     public var isHidingFollowCount: Bool {
         hideCollections ?? pleroma?.hideFollowsCount ?? false
     }
-    
+
     // todo - move
     public var isHidingFaves: Bool {
         pleroma?.hideFavorites ?? false
