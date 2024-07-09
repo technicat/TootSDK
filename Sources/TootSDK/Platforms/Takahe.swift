df405be1dea6ecb83cece78abb38c978f35db280
Philip Chu <technicat@users.noreply.github.com>
2024-07-08 23:02:41 -0600

    takahe doesn't support scheduled posts and lists

--- Sources/TootSDK/Platforms/Takahe.swift
+++ Sources/TootSDK/Platforms/Takahe.swift
@@ -1,3 +1,4 @@
+/// https://docs.jointakahe.org/en/latest/features/
 open class Takahe: MastoCompatible {
 
     open override var name: String { "Takahe" }
@@ -6,7 +7,16 @@ open class Takahe: MastoCompatible {
         // arbitrary mastodon api
         super.init(masto: Version(3, 5), platform: api)
     }
+    
+    // support
 
     /// doesn't support bot indicator in account settings
     open override var supportsBot: Bool { false }
+    
+    // limits
+    
+    /// lists not supported (can get but not create)
+    open override var listAccountsPageLimit: Int { 0 }
+    /// scheduled posts not supported
+    open override var scheduledPageLimit: Int { 0 }
 }
